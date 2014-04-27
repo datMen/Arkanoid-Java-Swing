4b31d5623bd208fa0fca151de632461d59d4284b
TheLouK <8thelouk8@gmail.com>
2014-04-27 10:25:51 +0100

    Pantalla class (swing)

--- src/Pantalla.java
+++ src/Pantalla.java
@@ -1,4 +1,42 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 
 public class Pantalla {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Pantalla window = new Pantalla();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Pantalla() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }

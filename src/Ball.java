a3969f60a4be70a71c01841b790fc2806d5ea027
TheLouK <8thelouk8@gmail.com>
2014-04-28 21:53:42 +0100

    Added collisions for ball

--- src/Ball.java
+++ src/Ball.java
@@ -1,10 +1,14 @@
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
 
 public class Ball {
-	int x = 0;
-	int y = 0;
+	private static final int DIAMETER = 10;
+	int x = 100;
+	int y = 100;
 	int xa = 1;
 	int ya = 1;
+	int brick;
 	private Pantalla game;
 
 	public Ball(Pantalla game) {
@@ -14,18 +18,49 @@ public class Ball {
 	void move() {
 		if (x + xa < 0)
 			xa = 1;
-		if (x + xa > game.getWidth() - 10)
+		else if (x + xa > game.getWidth() - DIAMETER)
 			xa = -1;
-		if (y + ya < 0)
+		else if (y + ya < 0)
 			ya = 1;
-		if (y + ya > game.getHeight() - 10)
+		else if (y + ya > game.getHeight() - DIAMETER)
+			game.gameOver();
+		else if (collision()){
+			System.out.println(game.bar.getBounds().intersection(getBounds()));
 			ya = -1;
-
+			y = game.bar.getTopY() - DIAMETER + 10;
+		}
+		else if (brickCollision()){
+			if (ya == 1) {
+				ya = -1;
+			}
+			else if (ya == -1) {
+				ya = 1;
+			}
+			Bricks.bricks.remove(brick);
+		}
 		x = x + xa;
 		y = y + ya;
 	}
 
+	private boolean collision() {
+		return game.bar.getBounds().intersects(getBounds());
+	}
+	
+	private boolean brickCollision() {
+		for (int i = 0; i < Bricks.bricks.size(); i++) {
+			if (Bricks.bricks.get(i).getBounds().intersects(getBounds())) {
+				brick = i;
+				return Bricks.bricks.get(i).getBounds().intersects(getBounds());
+			}
+		}
+		return false;
+	}
+
 	public void paint(Graphics2D g) {
 		g.fillOval(x, y, 10, 10);
+	}
+	
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, DIAMETER, DIAMETER);
 	}
 }

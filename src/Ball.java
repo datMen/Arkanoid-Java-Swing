e83778082c8c0edfc8c15df8cf4ccf2a5c5669db
TheLouK <8thelouk8@gmail.com>
2014-05-01 18:09:16 +0100

    [Ball] Improved hits and added more directions to the ball

--- src/Ball.java
+++ src/Ball.java
@@ -4,8 +4,8 @@ import java.awt.Rectangle;
 
 public class Ball {
 	private static final int DIAMETER = 10;
-	int x = 133;
-	int y = 325;
+	int x = 205;
+	int y = 400;
 	int xa = 0;
 	int ya = 0;
 	int brick;
@@ -18,44 +18,44 @@ public class Ball {
 
 	void move() {
 		if (x + xa < 0)
-			xa = 1;
+			xa *= -1;
 		else if (x + xa > game.getWidth() - DIAMETER)
-			xa = -1;
-		else if (y + ya < 0)
+			xa = -xa;
+		else if (y + ya < Text.menu_bar_height)
 			ya = 1;
 		else if (y + ya > game.getHeight() - DIAMETER)
 			game.gameOver();
 		else if (collision()){
 			ya = -1;
-			xa = sxa;
 			y = game.bar.getTopY() - DIAMETER + 10;
 		}
 		else if (brickCollision()){
-			int ball_position = (y+DIAMETER)-(Bricks.bricks.get(brick).getTopY()+10);
-			if (ball_position == 1 || ball_position == 19) {
-				if (ya == 1) {
-					ya = -1;
-				}
-				else if (ya == -1) {
-					ya = 1;
-				}
-			}
-			else {
-				if (xa == 1) {
-					xa = -1;
-				}
-				else if (xa == -1) {
-					xa = 1;
-				}
-			}
-			if (Bricks.bricks.get(brick).hits == 0) {
+			if (ultraballmode) {
 				Bricks.bricks.remove(brick);
-				if (Bricks.bricks.size() == 0) {
-					game.text.start_label.setText("Congrats, You Won!");
-				}
 			}
 			else {
-				game.brick.updateHits(brick);
+				int ball_position = (y+DIAMETER)-(Bricks.bricks.get(brick).getTopY()+10);
+				if (ball_position == 1 || ball_position == 19) {
+					if (ya == 1)
+						ya = -1;
+					else if (ya == -1)
+						ya = 1;
+				}
+				else {
+					if (xa > 0)
+						xa *= -1;
+					else if (xa < 0)
+						xa *= -1;
+				}
+				if (Bricks.bricks.get(brick).hits == 0) {
+					Bricks.bricks.remove(brick);
+					if (Bricks.bricks.size() == 0) {
+						game.text.start_label.setText("Congrats, You Won!");
+					}
+				}
+				else {
+					game.brick.updateHits(brick);
+				}
 			}
 		}
 		x = x + xa;
@@ -64,36 +64,37 @@ public class Ball {
 
 	private boolean collision() {
 		if (game.bar.getBounds().intersects(getBounds())) {
-			sxa = xa;
 			return true;
 		}
 		else if (game.bar.getLeftBounds().intersects(getBounds())) {
-			if (xa == 1) {
-				sxa = -1;
-				if (game.speed < 10) {
+			if (xa > 0) {
+				xa *= -1;
+				if (x*xa < 0 && xa+1 != 0)
+					xa++;
+				if (game.speed < 10)
 					game.speed += 1;
-				}
 			}
-			else if (xa == -1) {
-				sxa = -1;
-				if (game.speed > 1) {
+			else if (xa < 0) {
+				if (x*xa < 0 && xa-1 != 0)
+					xa--;
+				if (game.speed > 2)
 					game.speed -= 1;
-				}
 			}
 			return true;
 		}
 		else if (game.bar.getRightBounds().intersects(getBounds())) {
-			if (xa == -1) {
-				sxa = 1;
-				if (game.speed < 10) {
+			if (xa < 0) {
+				xa *= -1;
+				if (x*xa > 0 && xa-1 != 0)
+					xa--;
+				if (game.speed < 10)
 					game.speed += 1;
-				}
 			}
-			else if (xa == 1) {
-				sxa = 1;
-				if (game.speed > 1) {
+			else if (xa > 0) {
+				if (x*xa > 0 && xa+1 != 0)
+					xa++;
+				if (game.speed > 2)
 					game.speed -= 1;
-				}
 			}
 			return true;
 		}
